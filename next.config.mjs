be9bb4dcde81244019acba06ce7f1c46f8066615
AlexOrekhov <apexof@gmail.com>
2024-02-24 11:54:37 +0300

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
+    return {
+      ...config,
+      externals: [...config.externals, "pino-pretty", "lokijs", "encoding"],
+    };
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/apexof/pokemons/main/images/**",
+      },
+    ],
   },
 };
 
