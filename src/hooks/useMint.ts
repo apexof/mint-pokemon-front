acfac45d602b228e762a7cde4047a550c3503c0a
AlexOrekhov <apexof@gmail.com>
2024-03-08 21:29:07 +0300

    anybody can mint

--- src/hooks/useMint.ts
+++ src/hooks/useMint.ts
@@ -4,19 +4,14 @@ import { useAccount, useWriteContract } from 'wagmi'
 
 export const useMint = () => {
   const { writeContract, data: txHash, error: txHashError, isPending: txHashLoading } = useWriteContract()
-  const { address } = useAccount()
 
   const mint = useCallback(async () => {
-    if (!address) {
-      return
-    }
     writeContract({
       abi: pokemonFactory.abi,
       address: pokemonFactory.address,
       functionName: 'mint',
-      args: [address],
     })
-  }, [address])
+  }, [])
 
   return {
     txHashLoading,
