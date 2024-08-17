7ad13f957559e90b7ee7996c7f680d9a8c396baa
AlexOrekhov <apexof@gmail.com>
2024-08-17 12:57:39 +0400

    add polygon subgraph

--- src/widgets/PokemonList/const/graphUrls.ts
+++ src/widgets/PokemonList/const/graphUrls.ts
@@ -2,6 +2,6 @@ import { ChainId } from '@/entities/Chain'
 
 export const subgraphUrl: Record<ChainId, string> = {
   11_155_111: 'https://api.studio.thegraph.com/query/44553/pokemon-graph/version/latest',
-  137: '',
+  137: 'https://api.studio.thegraph.com/query/44553/pokemon-graph-polygon/version/latest',
   80_002: 'https://api.studio.thegraph.com/query/44553/pokemo-graph-amoy/version/latest',
 }
